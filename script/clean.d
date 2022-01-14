8540b8964a6f6e30f609d373d57adfedff2f1ae8
nonanonno <as.nonanonno@gmail.com>
2022-01-14 23:28:56 +0900

    fix: drop double brancket support (#45)
    * feat: sort out cleaning script
    
    * fix: drop double brancket `>>` support

--- script/clean.d
+++ script/clean.d
@@ -6,35 +6,62 @@ alias run = runImpl!workingDir;
 
 void main()
 {
-    const items = [
-        ".dub",
-        "rcld/.dub",
-        "rcld/ros2_d*",
-        "rcld/libros2_d*",
-        "rcl_bind/source/rcl/package.d",
-        "msg_gen/.dub",
-        "msg_gen/ros2_d*",
-        "msg_gen/libros2_d*",
-        "example/.dub",
-        "example/dub.selections.json",
-        "example/bin",
-        "example/example",
-        "example/ros2_d*",
-        "tests/msg_gen/.dub",
-        "tests/msg_gen/dub.selections.json",
-        "tests/msg_gen/tests-*",
-        "tests/rcld/.dub",
-        "tests/rcld/dub.selections.json",
-        "tests/rcld/tests-*",
-        "test_helper/.dub",
-        "test_helper/ament/build",
-        "test_helper/ament/install",
-        "test_helper/ament/log",
-        "test_helper/ament/src/test_interfaces",
-        "rcl_bind/.dub",
-        "rcl_bind/libros2_d_rcl_bind.a",
-        "docs"
+    auto items = ["docs"];
+
+    // packages
+    immutable packages = [
+        ".",
+        "msg_gen",
+        "rcl_bind",
+        "rcld",
+        "rosidl_parser",
+        "test_helper",
     ];
+
+    foreach (pkg; packages)
+    {
+        foreach (t; [".dub", "ros2_d*", "libros2_d*"])
+        {
+            items ~= buildPath(pkg, t);
+        }
+    }
+
+    // example
+    immutable examplePackagesTargets = [
+        "msg_gen": "msg_gen_example",
+        "rcl": "rcl_example",
+        "rcld": "bin",
+    ];
+    foreach (pkg, target; examplePackagesTargets)
+    {
+        foreach (t; [".dub", "dub.selections.json", target])
+        {
+            items ~= buildPath("example", pkg, t);
+        }
+    }
+
+    // tests
+    immutable testPackages = [
+        "msg_gen",
+        "rcld",
+    ];
+    foreach (pkg; testPackages)
+    {
+        foreach (t; [".dub", "dub.selections.json", "tests-*"])
+        {
+            items ~= buildPath("tests", pkg, t);
+        }
+    }
+
+    // ament
+    foreach (t; ["build", "install", "log", "src/test_interfaces"])
+    {
+        items ~= buildPath("test_helper", "ament", t);
+    }
+
+    // rcl_bind
+    items ~= buildPath("rcl_bind", "source", "rcl", "package.d");
+
     format!"rm -rf %-(%s %)"(items).run;
     "rm -rf -- *.lst".run;
 }
