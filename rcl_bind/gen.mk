067f0e5415e867871f8ee46bd1aac35ffc4be20d
nonanonno <as.nonanonno@gmail.com>
2022-05-14 19:31:39 +0900

    chore: add LDC env (#51)
    * chore: add LDC env
    
    * fix: suppress error
    
    From dmd 2.099.0, templates in the other library are not instanciated
    but it introduces link error in this project. To solve it, `-allinst`
    option is added.

--- rcl_bind/gen.mk
+++ rcl_bind/gen.mk
@@ -3,3 +3,5 @@ SRC = source/rcl/package.dpp
 
 $(DST): $(SRC)
 	CC=clang dub run -y dpp -- --preprocess-only --include-path /opt/ros/${ROS_DISTRO}/include source/rcl/package.dpp
+	sed -i -e '/struct _IO_FILE/,/}/d' source/rcl/package.d
+	sed -i -e '/module rcl/a import core.stdc.stdio;' source/rcl/package.d
